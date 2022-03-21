3462d3518d8ae30fe9adbda9af95e519d914bb59
KotKatLV <kotkat.lv@gmail.com>
2022-03-21 13:56:39 +0300

    83

---
+++ Rocky/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rocky.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null 
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // session exists
+                shoppingCarts = HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).ToList();
+            }
+
+            List<int> prodInCart = shoppingCarts.Select(p => p.ProductId).ToList();
+            IEnumerable<Product> productList = _db.Product.Where(p => prodInCart.Contains(p.Id));
+
+            return View(productList);
+        }
+    }
+}
