e6049c366081a286f4c750e8c4bdb384a3c0c0f7
KotKatLV <kotkat.lv@gmail.com>
2022-04-18 14:48:54 +0300

    42

--- Rocky/Controllers/InquiryController.cs
+++ Rocky/Controllers/InquiryController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rocky.DAL.Repository.Interfaces;
+using Rocky.Domain;
 using Rocky.UI.Web.ViewModels;
+using Rocky.Utils;
+using System.Collections.Generic;
 
 namespace Rocky.UI.Web.Controllers
 {
@@ -23,6 +26,7 @@ namespace Rocky.UI.Web.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Details(int id)
         {
             InquiryViewModel inquiryViewModel = new InquiryViewModel()
@@ -32,6 +36,29 @@ namespace Rocky.UI.Web.Controllers
             };
 
             return View(inquiryViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            InquiryViewModel.InquiryDetail = _inquiryDetailRepository.GetAll(u => u.InquiryHeaderId == InquiryViewModel.InquiryHeader.Id);
+
+            foreach (var detail in InquiryViewModel.InquiryDetail)
+            {
+                ShoppingCart shoppingCart = new()
+                {
+                    ProductId = detail.ProductId,
+                };
+                shoppingCartList.Add(shoppingCart);
+            }
+
+            HttpContext.Session.Clear();
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            HttpContext.Session.Set(WC.SessionInquiryId, InquiryViewModel.InquiryHeader.Id);
+
+            return RedirectToAction("Index", "Cart");
         }
 
         #region API CALLS
