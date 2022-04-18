77b5f7863edf18c5e9b4975f2ad182b9b50ea5a0
KotKatLV <kotkat.lv@gmail.com>
2022-04-18 12:01:41 +0300

    136

---
+++ Rocky/Controllers/InquiryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.DAL.Repository.Interfaces;
+
+namespace Rocky.UI.Web.Controllers
+{
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inquiryHeaderRepository;
+        private readonly IInquiryDetailRepository _inquiryDetailRepository;
+
+        public InquiryController(IInquiryHeaderRepository inquiryHeaderRepository, IInquiryDetailRepository inquiryDetailRepository)
+        {
+            _inquiryHeaderRepository = inquiryHeaderRepository;
+            _inquiryDetailRepository = inquiryDetailRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Rocky/ViewModels/InquiryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Rocky.Domain;
+using System.Collections.Generic;
+
+namespace Rocky.UI.Web.ViewModels
+{
+    public class InquiryViewModel
+    {
+        public InquiryHeader InquiryHeader { get; set; }
+
+        public IEnumerable<InquiryDetail> InquiryDetail { get; set; }
+    }
+}
