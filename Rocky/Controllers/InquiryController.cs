3984854e2998031a25b8cbb63fd2a4825f59480c
KotKatLV <kotkat.lv@gmail.com>
2022-04-18 13:07:42 +0300

    140

--- Rocky/Controllers/InquiryController.cs
+++ Rocky/Controllers/InquiryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rocky.DAL.Repository.Interfaces;
+using Rocky.UI.Web.ViewModels;
 
 namespace Rocky.UI.Web.Controllers
 {
@@ -7,6 +8,9 @@ namespace Rocky.UI.Web.Controllers
     {
         private readonly IInquiryHeaderRepository _inquiryHeaderRepository;
         private readonly IInquiryDetailRepository _inquiryDetailRepository;
+
+        [BindProperty]
+        public InquiryViewModel InquiryViewModel { get; set; }
 
         public InquiryController(IInquiryHeaderRepository inquiryHeaderRepository, IInquiryDetailRepository inquiryDetailRepository)
         {
@@ -17,6 +21,17 @@ namespace Rocky.UI.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            InquiryViewModel inquiryViewModel = new InquiryViewModel()
+            {
+                InquiryHeader = _inquiryHeaderRepository.FirstOrDefault(u => u.Id == id),
+                InquiryDetail = _inquiryDetailRepository.GetAll(u => u.InquiryHeaderId == id, includProps: "Product")
+            };
+
+            return View(inquiryViewModel);
         }
 
         #region API CALLS
