caaab8565102ccc1dce4e45806f9ac67e1dc33b2
Константин Константин <kotkat.lv@gmail.com>
2022-04-19 12:17:12 +0300

    59

--- Rocky.Domain/ApplicationUser.cs
+++ Rocky.Domain/ApplicationUser.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Rocky.Domain
 {
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; }
+
+        [NotMapped]
+        public string StreetAddress { get; set; }
+
+        [NotMapped]
+        public string City { get; set; }
+
+        [NotMapped]
+        public string State { get; set; }
+
+        [NotMapped]
+        public string PostalCode { get; set; }
     }
 }
