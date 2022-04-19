a4154afcf9fda11848f0d5d8496acf0936980eb1
Константин Константин <kotkat.lv@gmail.com>
2022-04-19 14:39:23 +0300

    167

---
+++ Rocky.Utils/BrainTree/BrainTreeSettinngs.cs
@@ -0,0 +1,13 @@
+﻿namespace Rocky.Utils.BrainTree
+{
+    public class BrainTreeSettinngs
+    {
+        public string Enviroment { get; set; }
+
+        public string MerchantID { get; set; }
+
+        public string PublicKey { get; set; }
+
+        public string PrivateKey { get; set; }
+    }
+}
---
+++ Rocky.Utils/BrainTree/IBrainTreeGate.cs
@@ -0,0 +1,11 @@
+﻿using Braintree;
+
+namespace Rocky.Utils.BrainTree
+{
+    public interface IBrainTreeGate
+    {
+        IBraintreeGateway CreateGateWay();
+
+        IBraintreeGateway GetGateWay();
+    }
+}
